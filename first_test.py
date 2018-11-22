e0d3edb3b4956e76d90710a3f8f64ce7db37fa13
AMogiley <45263311+AMogiley@users.noreply.github.com>
2018-11-22 17:47:26 +0300

    Update first_test.py

--- first_test.py
+++ first_test.py
@@ -1,27 +1,44 @@
+#!/usr/bin/python3
+		
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
-driver = webdriver.Chrome()
-driver.get("https://www.google.com/")
-elem = driver.find_element_by_name("q")
-elem.send_keys("selenide")
-elem.send_keys(Keys.RETURN)
-s_find = driver.find_element_by_xpath('//*[@id="rso"]/div[1]/div/div/div/div/div[1]/a/div/cite')
-if 'ru.selenide.org/' in s_find.text:
-	print("OK!!")
+		
+		
+class Search(unittest.TestCase):
 	
-driver.find_element_by_xpath('//*[@id="ow15"]/a').click()
-driver.find_element_by_xpath('//*[@id="lb"]/div/a[1]').click()
+	def setUp(self):			
+		self.driver = webdriver.Chrome()
 
-s_img = driver.find_element_by_xpath('//*[@id="rg_s"]/div[1]/a[2]/div[1]')
-print(s_img)
-print(s_img.text)
 
-if 'Selenide' or 'selenide' in s_img.text:
-    print('OK')
+	def test(self):
+		self.driver.get("https://www.google.com/")
+		assert 'Google' in self.driver.title		
+		elem = self.driver.find_element_by_name("q")
+		elem.send_keys("selenide")
+		elem.send_keys(Keys.RETURN)
+		s_find = self.driver.find_element_by_xpath('//*[@id="rso"]/div[1]/div/div/div/div/div[1]/a/div/cite')
+#		assert s_finde.text == 'ru.selenide.org'
+		if 'ru.selenide.org/' in s_find.text:
+			print("OK!!")
+			
+		self.driver.find_element_by_xpath('//*[@id="ow15"]/a').click()
+		self.driver.find_element_by_xpath('//*[@id="lb"]/div/a[1]').click()
+		
+		s_img = driver.find_element_by_xpath('//*[@id="rg_s"]/div[1]/a[2]/div[1]')
+		#print(s_img)
+		#print(s_img.text)
+		assert s_finde.text == 'ru.selenide.org/'		
+#		if 'Selenide' or 'selenide' in s_img.text:
+#		    print('OK')
+		
+		self.driver.back()
+		s_find = self.driver.find_element_by_xpath('//*[@id="rso"]/div[1]/div/div/div/div/div[1]/a/div/cite')
+		assert s_finde.text == 'ru.selenide.org/'
+#		if 'ru.selenide.org/' in s_find.text:
+#			print("OK!!")
+		
+	def tearDown(self):
+		self.driver.close()
 
-#driver.find_element_by_path('//*[@id="hdtb-msb-vis"]/div[1]/a').click()
-driver.back()
-s_find = driver.find_element_by_xpath('//*[@id="rso"]/div[1]/div/div/div/div/div[1]/a/div/cite')
-if 'ru.selenide.org/' in s_find.text:
-	print("OK!!")
+if __name__ == '__main__': unittest.main();
