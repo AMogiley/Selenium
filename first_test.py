5cd72584f65e30128a344b63a8ece88d8885e100
AMogiley <45263311+AMogiley@users.noreply.github.com>
2018-11-22 16:17:44 +0300

    Update first_test.py

--- first_test.py
+++ first_test.py
@@ -10,7 +10,18 @@ s_find = driver.find_element_by_xpath('//*[@id="rso"]/div[1]/div/div/div/div/div
 if 'ru.selenide.org/' in s_find.text:
 	print("OK!!")
 	
-mor = driver.find_element_by_xpath('//*[@id="ow15"]/a')
-mor.send_keys(Keys.RETURN)
-s_img = driver.find_element_by_xpath('//*[@id="lb"]/div/a[1]')
-s_img.send_keys(Keys.RETURN)
+driver.find_element_by_xpath('//*[@id="ow15"]/a').click()
+driver.find_element_by_xpath('//*[@id="lb"]/div/a[1]').click()
+
+s_img = driver.find_element_by_xpath('//*[@id="rg_s"]/div[1]/a[2]/div[1]')
+print(s_img)
+print(s_img.text)
+
+if 'Selenide' or 'selenide' in s_img.text:
+    print('OK')
+
+#driver.find_element_by_path('//*[@id="hdtb-msb-vis"]/div[1]/a').click()
+driver.back()
+s_find = driver.find_element_by_xpath('//*[@id="rso"]/div[1]/div/div/div/div/div[1]/a/div/cite')
+if 'ru.selenide.org/' in s_find.text:
+	print("OK!!")
