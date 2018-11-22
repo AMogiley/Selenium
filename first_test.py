0765cf69500ec0456e6c87b34b21873e05b563ed
mCstl <37896336+mCstl@users.noreply.github.com>
2018-11-22 13:20:18 +0300

    Update first_test.py

--- first_test.py
+++ first_test.py
@@ -2,20 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
-#
 driver.get("https://www.google.com/")
-#assert "Python" in driver.title
 elem = driver.find_element_by_name("q")
 elem.send_keys("selenide")
 elem.send_keys(Keys.RETURN)
-s_find = driver.find_element_by_xpath("/html/body/div[7]/div[3]/div[10]/div[1]/div[2]/div/div[2]/div[2]/div/div/div/div[1]/div/div/div/div/div[1]/a/div/cite")
-print("-----------------------------------")
-print(s_find)
-print("-----------------------------------")
-#if "selenide.org" in s_finde:
-#	print("selenide.org на месте : )")
-#assert "No results found." not in driver.page_source
-#driver.close()
-
-
-#
+s_find = driver.find_element_by_xpath('//*[@id="rso"]/div[1]/div/div/div/div/div[1]/a/div/cite')
+if 'ru.selenide.org/' in s_find.text:
+	print("OK!!")
+	
+mor = driver.find_element_by_xpath('//*[@id="ow15"]/a')
+mor.send_keys(Keys.RETURN)
+s_img = driver.find_element_by_xpath('//*[@id="lb"]/div/a[1]')
+s_img.send_keys(Keys.RETURN)
