704dbc7491a6fda769ea16d69bc34457c963016f
AMogiley <45263311+AMogiley@users.noreply.github.com>
2018-11-23 17:00:55 +0300

    Update final.py

--- final.py
+++ final.py
@@ -11,7 +11,7 @@ class Search(unittest.TestCase):
 		self.driver = webdriver.Chrome()
 		
 	def test(self):
-		self.driver.get("https://www.google.com/")	#открыть google и проверить валидность 
+		self.driver.get("https://www.google.com/")	# открыть google и проверить валидность 
 		assert 'Google' in self.driver.title		
 		elem = self.driver.find_element_by_name("q")	# найти строку поиска для ввода
 		elem.send_keys("selenide")
@@ -20,7 +20,7 @@ class Search(unittest.TestCase):
 		assert 'ru.selenide.org/' in s_find.text 
 		
 		self.driver.find_element_by_xpath('//*[@id="ow15"]/a').click()
-		self.driver.find_element_by_xpath('//*[@id="lb"]/div/a[1]').click()	# зайти во вкладку картинки через кнопку ещё (почему то версия страницы google переодически менялась, но потом стала выходить только страница где вкладка картинки открывается через кнопку Ещё)
+		self.driver.find_element_by_xpath('//*[@id="lb"]/div/a[1]').click()	# зайти во вкладку картинки через кнопку ещё (почему то версия страницы google переодически менялась, но потом стал выходить только один тип страницы где вкладка "картинки" открывается через кнопку Ещё)
 
 		s_img = self.driver.find_element_by_xpath('//*[@id="rg_s"]/div[1]/a[2]/div[2]')
 		print(s_img.text)
